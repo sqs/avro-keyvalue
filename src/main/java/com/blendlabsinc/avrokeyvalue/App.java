8fc650e6bc6a0e381d6da9ae7e543b1970c44f54
Quinn Slack <qslack@qslack.com>
2012-07-18 05:13:19 -0700

    initial Maven layout + pom.xml

---
+++ src/main/java/com/blendlabsinc/avrokeyvalue/App.java
@@ -0,0 +1,13 @@
+package com.blendlabsinc.avrokeyvalue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
